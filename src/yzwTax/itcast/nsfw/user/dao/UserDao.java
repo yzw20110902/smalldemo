40adbf6e94dd0b2921ca25d19b6788ad8c797040
50188 <50188@YU-PC>
2017-05-02 18:12:18 +0800

    1.增加用户接口和实现接口方法

---
+++ src/yzwTax/itcast/nsfw/user/dao/UserDao.java
@@ -0,0 +1,8 @@
+package yzwTax.itcast.nsfw.user.dao;
+
+import yzwTax.itcast.core.dao.BaseDao;
+import yzwTax.itcast.nsfw.user.entity.User;
+
+public interface UserDao extends BaseDao<User> {
+
+}
---
+++ src/yzwTax/itcast/nsfw/user/dao/impl/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package yzwTax.itcast.nsfw.user.dao.impl;
+
+import yzwTax.itcast.core.dao.impl.BaseDaoImpl;
+import yzwTax.itcast.nsfw.user.dao.UserDao;
+import yzwTax.itcast.nsfw.user.entity.User;
+
+public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao {
+
+}
