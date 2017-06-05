8dd7e541725ea6d0c599c34c1e9dc37ec0217e17
50188 <50188@YU-PC>
2017-06-05 09:52:12 +0800

    1.增加用户角色权限实体 2.增加日期选择控件

---
+++ src/yzwTax/itcast/nsfw/user/entity/UserRole.java
@@ -0,0 +1,26 @@
+package yzwTax.itcast.nsfw.user.entity;
+
+import java.io.Serializable;
+
+public class UserRole implements Serializable {
+
+	private UserRoleId id;
+
+	public UserRole() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserRole(UserRoleId id) {
+
+		this.id = id;
+	}
+
+	public UserRoleId getId() {
+		return id;
+	}
+
+	public void setId(UserRoleId id) {
+		this.id = id;
+	}
+
+}
